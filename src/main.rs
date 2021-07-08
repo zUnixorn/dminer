c461b212f2e5cbbcf47e1fb841e0b523c373846b
flamion <laberschleichi@gmail.com>
2021-07-08 10:47:11 +0200

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,10 @@ use serenity::{
 async fn main() {
 	let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-	let mut client =
-		Client::builder(&token).event_handler(Handler).await.expect("Err creating client");
+	let mut client = Client::builder(&token)
+			.event_handler(Handler)
+			.await
+			.expect("Err creating client");
 
 	if let Err(why) = client.start().await {
 		println!("Client error: {:?}", why);
