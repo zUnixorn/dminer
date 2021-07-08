603b740df74b8a21cc0875bd41c9ac1d454ff4f5
zUnixorn <jonas.crull@gmx.de>
2021-07-08 08:46:04 +0200

    initialized repository

---
+++ dminer_data_localhost-2021_07_08_08_45_44-dump.sql
@@ -0,0 +1,107 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.3
+-- Dumped by pg_dump version 13.3
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'SQL_ASCII';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: activity_type; Type: TYPE; Schema: public; Owner: dminer
+--
+
+CREATE TYPE public.activity_type AS ENUM (
+    'Playing',
+    'Streaming',
+    'Listening',
+    'Watching',
+    'Custom',
+    'Competing',
+    'Unknown'
+);
+
+
+ALTER TYPE public.activity_type OWNER TO dminer;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: activities; Type: TABLE; Schema: public; Owner: dminer
+--
+
+CREATE TABLE public.activities (
+    user_id bigint,
+    application_id bigint,
+    assets text,
+    details text,
+    kind public.activity_type NOT NULL,
+    activity_name text NOT NULL,
+    party boolean NOT NULL,
+    state text,
+    start_timestamp bigint NOT NULL,
+    end_timestamp bigint
+);
+
+
+ALTER TABLE public.activities OWNER TO dminer;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: dminer
+--
+
+CREATE TABLE public.users (
+    user_id bigint NOT NULL,
+    username text
+);
+
+
+ALTER TABLE public.users OWNER TO dminer;
+
+--
+-- Data for Name: activities; Type: TABLE DATA; Schema: public; Owner: dminer
+--
+
+COPY public.activities (user_id, application_id, assets, details, kind, activity_name, party, state, start_timestamp, end_timestamp) FROM stdin;
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: dminer
+--
+
+COPY public.users (user_id, username) FROM stdin;
+\.
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: dminer
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: activities activities_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: dminer
+--
+
+ALTER TABLE ONLY public.activities
+    ADD CONSTRAINT activities_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
