5997dbde5c1bde1427123f588de7e436656e3e85
flamion <laberschleichi@gmail.com>
2021-07-15 13:34:58 +0200

    Fixed help description and aliases in math.rs

--- src/commands/math.rs
+++ src/commands/math.rs
@@ -19,7 +19,7 @@ pub async fn add(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult 
 
 #[command]
 #[aliases("*")]
-#[description("adds 2 numbers\nUsage: add number1 number2\nExample: add 2 2")]
+#[description("multiply 2 numbers\nExample: multiply 2 2")]
 pub async fn multiply(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 	let first = args.single::<f64>()?;
 	let second = args.single::<f64>()?;
@@ -33,7 +33,7 @@ pub async fn multiply(ctx: &Context, msg: &Message, mut args: Args) -> CommandRe
 
 #[command]
 #[aliases("/")]
-#[description("adds 2 numbers\nUsage: add number1 number2\nExample: add 2 2")]
+#[description("divide 2 numbers\nExample: divide 2 2")]
 pub async fn divide(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 	let first = args.single::<f64>()?;
 	let second = args.single::<f64>()?;
@@ -46,8 +46,8 @@ pub async fn divide(ctx: &Context, msg: &Message, mut args: Args) -> CommandResu
 }
 
 #[command]
-#[aliases("+")]
-#[description("adds 2 numbers\nUsage: add number1 number2\nExample: add 2 2")]
+#[aliases("-")]
+#[description("subtracts 2 numbers\nExample: subtract 2 2")]
 pub async fn subtract(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 	let first = args.single::<f64>()?;
 	let second = args.single::<f64>()?;
@@ -61,7 +61,7 @@ pub async fn subtract(ctx: &Context, msg: &Message, mut args: Args) -> CommandRe
 
 #[command]
 #[aliases("repl")]
-#[description("adds 2 numbers\nUsage: add number1 number2\nExample: add 2 2")]
+#[description("Evaluates a given mathematical expression\nExample: eval 2 - 3 * log(24/3) ^ 5")]
 pub async fn eval(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
 	let mut namespace = fasteval::EmptyNamespace; //no variables therefore empty namespace
 
