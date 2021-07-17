50de66dda62db3b5221e27145d04715e6784c4ce
flammenderdrache <laberschleichi@gmail.com>
2021-07-17 02:13:38 +0200

    Renamed queue to play

--- src/commands/music.rs
+++ src/commands/music.rs
@@ -112,54 +112,6 @@ async fn play(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 	let url = match args.single::<String>() {
 		Ok(url) => url,
 		Err(_) => {
-			msg.channel_id.say(&ctx.http, "Must provide a URL to a video or audio").await?;
-
-			return Ok(());
-		}
-	};
-
-	if !url.starts_with("http") {
-		msg.channel_id.say(&ctx.http, "Must provide a valid URL").await?;
-
-		return Ok(());
-	}
-
-	let guild = msg.guild(&ctx.cache).await.unwrap();
-	let guild_id = guild.id;
-
-	let manager = songbird::get(ctx).await
-		.expect("Songbird Voice client placed in at initialisation.").clone();
-
-	if let Some(handler_lock) = manager.get(guild_id) {
-		let mut handler = handler_lock.lock().await;
-
-		let source = match songbird::ytdl(&url).await {
-			Ok(source) => source,
-			Err(why) => {
-				println!("Err starting source: {:?}", why);
-
-				msg.channel_id.say(&ctx.http, "Error sourcing ffmpeg").await?;
-
-				return Ok(());
-			}
-		};
-
-		handler.play_source(source);
-
-		msg.channel_id.say(&ctx.http, "Playing song").await?;
-	} else {
-		msg.channel_id.say(&ctx.http, "Not in a voice channel to play in").await?;
-	}
-
-	Ok(())
-}
-
-#[command]
-#[only_in(guilds)]
-async fn queue(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-	let url = match args.single::<String>() {
-		Ok(url) => url,
-		Err(_) => {
 			msg.channel_id
 				.say(&ctx.http, "Must provide a URL to a video or audio")
 				.await?;
--- src/message_processing.rs
+++ src/message_processing.rs
@@ -24,7 +24,7 @@ pub struct General;
 struct Math;
 
 #[group]
-#[commands(join, leave, play, queue, skip)]
+#[commands(join, leave, play, skip)]
 pub struct Music;
 
 #[hook]
