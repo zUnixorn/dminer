123c2b11f713773a8282d0c76479f17dc9c372e4
flamion <laberschleichi@gmail.com>
2021-07-20 10:57:30 +0200

    Added remove track from queue feature

--- src/commands/music.rs
+++ src/commands/music.rs
@@ -321,6 +321,32 @@ async fn queue(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 }
 
 #[command]
+#[num_args(1)]
+async fn remove(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+	let data = ctx.data.read().await;
+	let lava_client = data.get::<Lavalink>().unwrap().clone();
+	{
+		if let Some(mut node) = lava_client.nodes().await.get_mut(msg.guild_id.unwrap().as_u64()) {
+			let queue = &mut node.queue;
+			if let Ok(index) = args.single::<usize>() {
+				if index > queue.len() || index < 1 {
+					msg.channel_id.say(&ctx.http, "Invalid queue index").await?;
+				} else {
+					let removed_track = queue.remove(index);
+					msg.channel_id.say(
+						&ctx.http,
+						format!("Removed track `{}` from the queue", removed_track.track.info.unwrap().title)
+					).await?;
+				}
+			}
+		}
+	}
+
+
+	Ok(())
+}
+
+#[command]
 async fn clear(ctx: &Context, msg: &Message) -> CommandResult {
 	let data = ctx.data.read().await;
 	let lava_client = data.get::<Lavalink>().unwrap().clone();
