b83b3fde2b2e54b7429835e2784cc01c2fa7aa24
flamion <laberschleichi@gmail.com>
2021-07-20 12:02:25 +0200

    Add remove track command to music group

--- src/message_processing.rs
+++ src/message_processing.rs
@@ -29,7 +29,7 @@ struct Math;
 #[cfg(feature = "music")]
 #[group]
 #[only_in(guilds)]
-#[commands(join, leave, play, skip, info, queue, clear, pause, unpause)]
+#[commands(join, leave, play, skip, info, queue, clear, pause, unpause, remove)]
 pub struct Music;
 
 #[hook]
