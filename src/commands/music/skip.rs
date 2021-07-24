9c3871cc3b3db9a9e0ae4ab99b1f9df7bbdcc0bb
flammenderdrache <laberschleichi@gmail.com>
2021-07-25 00:35:53 +0200

    Skip command now skips the player if the queue is empty

--- src/commands/music/skip.rs
+++ src/commands/music/skip.rs
@@ -10,17 +10,22 @@ use crate::commands::music::handlers::Lavalink;
 async fn skip(ctx: &Context, msg: &Message) -> CommandResult {
 	let data = ctx.data.read().await;
 	let lava_client = data.get::<Lavalink>().unwrap().clone();
+	let guild_id = msg.guild_id.unwrap();
 
 	if let Some(track) = lava_client.skip(msg.guild_id.unwrap()).await {
+		if let Some(node) = lava_client.nodes().await.get(guild_id.as_u64()) {
+			if node.queue.len() == 0 {
+				lava_client.stop(guild_id).await?;
+			}
+		}
 		msg.channel_id
 			.say(
 				ctx,
 				format!("Skipped: {}", track.track.info.as_ref().unwrap().title),
 			)
 			.await?;
-	} else {
-		msg.channel_id.say(ctx, "Nothing to skip.").await?;
 	}
+
 
 	Ok(())
 }
