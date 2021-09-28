a531eed91fe9eb35f4c0401a1cb69020b6247bd4
flammenderdrache <laberschleichi@gmail.com>
2021-09-28 18:59:48 +0200

    Removed additional message when a song begins to play

--- src/commands/music/handlers.rs
+++ src/commands/music/handlers.rs
@@ -17,22 +17,7 @@ use crate::commands::music::util::CallerChannel;
 
 #[async_trait]
 impl LavalinkEventHandler for LavalinkHandler {
-	async fn track_start(&self, client: LavalinkClient, event: TrackStart) {
-		//Use the data inside the Node Struct to store a Channel ID in the data Typemap field
-		if let Some(node) = client.nodes().await.get(&event.guild_id) {
-			if let Some(current_track) = &node.now_playing {
-				let typemap = node.data.read().await;
-				let caller_channel = typemap.get::<CallerChannel>().unwrap();
-				let _ = caller_channel.channel_id.send_message(&caller_channel.http, |message| {
-					message.embed(|embed| {
-						embed.title("**Now playing**");
-						embed.description(format!("{}", current_track.track.info.as_ref().unwrap().title));
-						embed
-					})
-				},
-				).await;
-			}
-		}
+	async fn track_start(&self, _client: LavalinkClient, event: TrackStart) {
 		log::info!("A track in guild {} was started!", event.guild_id);
 	}
 	async fn track_finish(&self, _client: LavalinkClient, event: TrackFinish) {
